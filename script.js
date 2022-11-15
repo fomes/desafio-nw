b562c94e4265d2e77ea4f2d7289258880a849b66
fomes <fgomesdeluna@gmail.com>
2022-11-15 13:58:44 -0300

    finish web layout + dropdown menu

---
+++ script.js
@@ -0,0 +1,12 @@
+const openDropdown = () => {
+  document.getElementById("sites-drop").classList.toggle("show");
+};
+
+window.onclick = (event) => {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown");
+    if (dropdowns.classList.contains("show")) {
+      dropdowns.classList.remove("show");
+    }
+  }
+};
