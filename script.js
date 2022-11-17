52b10beeb64ace665c825ebfd41ee42809ab04ec
fomes <fgomesdeluna@gmail.com>
2022-11-16 22:54:26 -0300

    start swiper implementation

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ window.onclick = (event) => {
     }
   }
 };
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  cssMode: true,
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
